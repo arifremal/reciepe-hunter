f60dca2c55108c33056af35e4a37f5842a8b4c52
arifremal <arifremal013@gmail.com>
2023-05-04 14:40:42 +0600

    active route indicator added

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -1,23 +1,26 @@
 import React, { useContext } from "react";
 import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProviders";
 import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
-  const {user, logOut}=  useContext(AuthContext)
-  const handleLogOut =()=>{
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
     logOut()
-    .then ()
-    .catch(error =>{
-      console.log(error);
-    })
-  }
+      .then()
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Container fluid>
-          <Navbar.Brand > <strong>Next</strong> Chef </Navbar.Brand>
+          <Navbar.Brand>
+            {" "}
+            <strong>Next</strong> Chef{" "}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -25,27 +28,53 @@ const Header = () => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Link className="text-decoration-none " to={"/"}>HOME</Link>
-              <Link className="text-decoration-none ps-2" to={"/blog"}>BLOG</Link>
-              
-         
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-danger text-decoration-none ps-2"
+                    : "text-decoration-none ps-2"
+                }
+              >
+                Home
+              </NavLink>
+              {/* <Link className="text-decoration-none ps-2" to={"/blog" }>BLOG</Link> */}
+              <NavLink
+                to="/blog"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-danger text-decoration-none ps-2"
+                    : "text-decoration-none ps-2"
+                }
+              >
+                Blog
+              </NavLink>
             </Nav>
-           <Nav>
-           { user && <Nav.Link>
-              {/* <FaUserCircle style={{fontSize:'2rem'}} ></FaUserCircle> */}
-            <img style={{ height: '40px' }} className="rounded-circle" src={user.photoURL} alt="" />
-            </Nav.Link>}
-           </Nav>
-             { user ? 
-             <Button onClick={handleLogOut} variant="outline-primary">Logout</Button>:
-           <Link to='/login' >
-             <Button variant="outline-primary">Login</Button>
-           </Link>
-            }
+            <Nav>
+              {user && (
+                <Nav.Link>
+                  {/* <FaUserCircle style={{fontSize:'2rem'}} ></FaUserCircle> */}
+                  <img
+                    style={{ height: "40px" }}
+                    className="rounded-circle"
+                    src={user.photoURL}
+                    alt=""
+                  />
+                </Nav.Link>
+              )}
+            </Nav>
+            {user ? (
+              <Button onClick={handleLogOut} variant="outline-primary">
+                Logout
+              </Button>
+            ) : (
+              <Link to="/login">
+                <Button variant="outline-primary">Login</Button>
+              </Link>
+            )}
             {/* <Link to="registration">
             <Button className="mx-2" variant="primary">Registration</Button>
             </Link> */}
-          
           </Navbar.Collapse>
         </Container>
       </Navbar>
